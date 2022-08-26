29d1eb4973841e769f82e9e7c181c8f601766c6a
sriram1702 <ramsri1702@gmail.com>
2022-08-26 12:47:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import './App.css'
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 function App() {
+  const [text,setText] = useState('');
+  const {speak} = useSpeechSynthesis();
+
+  const handleOnClick = () => {
+    speak({text:text})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <>
+        <h1>Text to Speech </h1>
+        <textarea className="textAreaStyle" onChange={(e) => { setText(e.target.value); } }></textarea>
+        <button className="buttonStyle" onClick={() => { handleOnClick(); } }>Listen</button></>
+    
   );
 }
 
